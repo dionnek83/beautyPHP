c0d3ea2b3c8fb46ad6b77e9a1618e381a417be24
dionnek83 <dionnek83@gmail.com>
2022-08-08 10:56:27 +0200

    added headers to config.php

--- beautyPHP/api/config.php
+++ beautyPHP/api/config.php
@@ -3,6 +3,9 @@
 // header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 
 
 //MySql server and database info
